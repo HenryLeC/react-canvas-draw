8eda7d161e1fed4f4f92b55e315ba0e9ec5744ed
Henry LeCompte <henry@volunteerio.us>
2020-11-27 17:49:52 -0500

    Added Old Files

---
+++ src/index.d.ts
@@ -0,0 +1,48 @@
+// Type definitions for @henrylec/react-canvas-draw 1.1
+
+import * as React from 'react';
+
+export interface CanvasDrawProps {
+    onChange?: ((canvas: CanvasDraw) => void) | null;
+    loadTimeOffset?: number;
+    lazyRadius?: number;
+    brushRadius?: number;
+    brushColor?: string;
+    catenaryColor?: string;
+    gridColor?: string;
+    backgroundColor?: string;
+    hideGrid?: boolean;
+    canvasWidth?: number | string;
+    canvasHeight?: number | string;
+    disabled?: boolean;
+    imgSrc?: string;
+    saveData?: string;
+    immediateLoading?: boolean;
+    hideInterface?: boolean;
+    className?: string;
+    style?: React.CSSProperties;
+    erase?: boolean
+}
+
+export default class CanvasDraw extends React.Component<CanvasDrawProps> {
+    /**
+     * Returns the drawing's save-data as a stringified object.
+     */
+    getSaveData(): string;
+
+    /**
+     * Loads a previously saved drawing using the saveData string, as well as an optional boolean
+     * flag to load it immediately, instead of live-drawing it.
+     */
+    loadSaveData(saveData: string, immediate?: boolean): void;
+
+    /**
+     * Clears the canvas completely.
+     */
+    clear(): void;
+
+    /**
+     * Removes the latest change to the drawing. This includes everything drawn since the last MouseDown event.
+     */
+    undo(): void;
+}
